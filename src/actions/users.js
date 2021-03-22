9a79e21b0cd9c5c1cd6eb4ed213daf348e4ad981
rowjay007 <rowlives@gmail.com>
2021-03-22 11:00:26 +0100

    create proivder store in index file to handle reducer

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,8 @@
-export cont RECEIVE_USERS = "RECEIVE_USERS"
+export const RECEIVE_USERS = "RECEIVE_USERS";
 
-export function receiveUsers (users ) {
-    return {
-        type: RECEIVE_USER,
-        users,
-    }
+export function receiveUsers(users) {
+  return {
+    type: RECEIVE_USERS,
+    users,
+  };
 }
