2a940ec4e7c5e658e66e974bd38c9fcd665809c6
rowjay007 <rowlives@gmail.com>
2021-03-24 07:36:42 +0100

    create toggleTweet in reducer

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,4 +1,7 @@
+import { saveLikeToggle } from "../utils/api";
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOGGLE_TWEET = "TOGGLE_TWEET";
 
 export function receiveTweets(tweets) {
   return {
@@ -6,3 +9,15 @@ export function receiveTweets(tweets) {
     tweets,
   };
 }
+
+function toggleTweet({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  };
+}
+
+
+
